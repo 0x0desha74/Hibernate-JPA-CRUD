bbdf05c5b756da5d19bc6ac274d8c34bc89902bd
0x0desha74 <mudesha74@gmail.com>
2025-04-24 16:53:36 +0200

    Saving Java object with Hibernate/JPA

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,10 +14,21 @@ public class CruddemoApplication {
         SpringApplication.run(CruddemoApplication.class, args);
     }
 
+    //Injecting StudentDAO
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("Hello, World!");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        //Creating the student object
+        System.out.println("Creating new student object...");
+        Student student = new Student("Mustafa","Elsayed","mustafa.elsayed@gmail.com");
+        //Save student object
+        studentDAO.save(student);
+        // Display id of the  saved student
+        System.out.println(student.getId());
     }
 }
---
+++ src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,21 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+    //inject entity manager
+    private final EntityManager entityManager;
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    @Override
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+}
